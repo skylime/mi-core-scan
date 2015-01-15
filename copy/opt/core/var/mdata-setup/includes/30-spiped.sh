54e0576bba5567e1cf58f90f3a277dca7150de8f
Boris Poblotzki <bp@core.io>
2015-01-16 00:36:38 +0100

    minor fixes to connect this machine via spipes

--- copy/opt/core/var/mdata-setup/includes/30-spiped.sh
+++ copy/opt/core/var/mdata-setup/includes/30-spiped.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Configure spiped for spamd and scand connection
 
-/opt/core/bin/spiped-configure-smf core-scan decrypt [::]:53310   [::1]:3310     $(mdata-get scan_key)
-/opt/core/bin/spiped-configure-smf core-scan decrypt [::]:51133   [::1]:11133    $(mdata-get scan_key)
+/opt/core/bin/spiped-configure-smf core-clam decrypt [0.0.0.0]:53310   [127.0.0.1]:3310     $(mdata-get scan_key)
+/opt/core/bin/spiped-configure-smf core-spam decrypt [0.0.0.0]:51133   [127.0.0.1]:11133    $(mdata-get scan_key)
