d6cac55a943f2403056f99e1beabcbfc7be9bcd1
Boris Poblotzki <bp@core.io>
2015-01-19 17:16:17 +0100

    spipe ends for scanner noch for IPv6 too

--- copy/opt/core/var/mdata-setup/includes/30-spiped.sh
+++ copy/opt/core/var/mdata-setup/includes/30-spiped.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Configure spiped for spamd and scand connection
 
-/opt/core/bin/spiped-configure-smf core-clam decrypt [0.0.0.0]:53310   [127.0.0.1]:3310     $(mdata-get scan_key)
-/opt/core/bin/spiped-configure-smf core-spam decrypt [0.0.0.0]:51133   [127.0.0.1]:11133    $(mdata-get scan_key)
+# SPIPES for clamd and spamd for IPv4...
+/opt/core/bin/spiped-configure-smf core-clam decrypt [0.0.0.0]:53310  [127.0.0.1]:3310  $(mdata-get scan_key)
+/opt/core/bin/spiped-configure-smf core-spam decrypt [0.0.0.0]:51133  [127.0.0.1]:11133 $(mdata-get scan_key)
+# ...and for IPv6 - if it fails it's included in getting a socket for 0.0.0.0 for compatibility
+/opt/core/bin/spiped-configure-smf core-clam decrypt [::]:53310       [127.0.0.1]:3310  $(mdata-get scan_key) 2>/dev/null
+/opt/core/bin/spiped-configure-smf core-spam decrypt [::]:51133       [127.0.0.1]:11133 $(mdata-get scan_key) 2>/dev/null
